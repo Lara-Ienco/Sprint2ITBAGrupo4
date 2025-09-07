182c9c0d66d7f994da1dccefa6d4bf19c33b5725
MaximoNorry <maxiurcmonaco@gmail.com>
2025-09-07 20:57:34 -0300

    Actualizo los precios en productos destacados

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const productos2 = [
-  { id: 1, nombre: "Escritorio Costa", precio: 12000, imagen: "imagenes/Escritorio Costa.png" },
-  { id: 2, nombre: "Sofa Patagonia", precio: 45000, imagen: "imagenes/Sofá Patagonia.png" },
-  { id: 3, nombre: "Butaca Mendoza", precio: 28000, imagen: "imagenes/Butaca Mendoza.png" },
-  { id: 4, nombre: "Mesa de comedor Pampa", precio: 60000, imagen: "imagenes/Mesa Comedor Pampa.png" },
-  { id: 5, nombre: "Mesa de centro Araucaria", precio: 15000, imagen: "imagenes/Mesa de Centro Araucaria.png" },
-  { id: 6, nombre: "Sillon Copacabana", precio: 35000, imagen: "imagenes/sillón copacabana.png" },
-  { id: 7, nombre: "Aparador Uspallata", precio: 9000, imagen: "imagenes/Aparador Uspallata.png" },
-  { id: 8, nombre: "Biblioteca Recoleta", precio: 75000, imagen: "imagenes/Biblioteca Recoleta.png"}
+  { id: 1, nombre: "Escritorio Costa", precio: 200000, imagen: "imagenes/Escritorio Costa.png" },
+  { id: 2, nombre: "Sofa Patagonia", precio: 800000, imagen: "imagenes/Sofá Patagonia.png" },
+  { id: 3, nombre: "Butaca Mendoza", precio: 250000, imagen: "imagenes/Butaca Mendoza.png" },
+  { id: 4, nombre: "Mesa de comedor Pampa", precio: 680000, imagen: "imagenes/Mesa Comedor Pampa.png" },
+  { id: 5, nombre: "Mesa de centro Araucaria", precio: 320000, imagen: "imagenes/Mesa de Centro Araucaria.png" },
+  { id: 6, nombre: "Sillon Copacabana", precio: 600000, imagen: "imagenes/sillón copacabana.png" },
+  { id: 7, nombre: "Aparador Uspallata", precio: 450000, imagen: "imagenes/Aparador Uspallata.png" },
+  { id: 8, nombre: "Biblioteca Recoleta", precio: 380000, imagen: "imagenes/Biblioteca Recoleta.png"}
 ];
 
 function initCarousel() {
